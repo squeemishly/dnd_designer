870eaba80060f00c0e8b43c7f215d45734679838
squeemishly <squeemishly@gmail.com>
2018-01-17 14:51:43 -0700

    add cookie session

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
 const express = require("express");
 const passport = require("passport");
+const cookieSession = require("cookie-session");
+const environment = process.env.NODE_ENV || 'development';
+const configuration = require('./knexfile')[environment];
+const database = require('knex')(configuration);
+const keys = require("./config/keys")
 require("./services/passport");
 
 const app = express();
+
+app.use(
+  cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+    keys: [keys.cookieKey]
+  })
+)
 
 app.get("/", (req, res) => {
   res.send({ hi: "there" });
