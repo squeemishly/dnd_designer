2f3ef57b7e3ff381faf883c9b69739030c41442e
squeemishly <squeemishly@gmail.com>
2018-01-18 12:39:40 -0700

    add modal reference

--- client/src/components/CharacterNew.js
+++ client/src/components/CharacterNew.js
@@ -1,29 +1,28 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
+import Modal from "./UI/Modal/Modal"
+import Aux from "../hoc/Aux/Aux"
 
 class CharacterNew extends Component {
   state = {
-    characters: [
-      "Dwarf",
-      "Elf",
-      "Halfling"
-    ]
-  }
+    characters: ["Dwarf", "Elf", "Halfling"]
+  };
 
   render() {
-    return(
-      <div>
-      <h1>CHOOSE YOUR RACE:</h1>
-        <ul>
-        {this.state.characters.map( character => (
-            <li>
-              {character}
-            </li>
-        ))}
-        </ul>
-      </div>
-    )
+    return (
+      <Aux>
+        <Modal>
+          Modal Information
+        </Modal>
+
+        <div>
+          <h1>CHOOSE YOUR RACE:</h1>
+          <h6>Click a character race for more information</h6>
+          <ul>{this.state.characters.map(character => <li>{character}</li>)}</ul>
+        </div>
+      </Aux>
+    );
   }
 }
 
-export default CharacterNew
+export default CharacterNew;
