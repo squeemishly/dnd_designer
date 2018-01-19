35855ee1030288c974ebf2f883f0ded1c1daf318
Benjamin Ross <benjaminross1001@gmail.com>
2018-01-19 09:55:51 -0700

    update knexfile production configuration

--- knexfile.js
+++ knexfile.js
@@ -1,37 +1,37 @@
 module.exports = {
   development: {
-    client: 'pg',
-    connection:'postgres://localhost/dnd_designer',
+    client: "pg",
+    connection: "postgres://localhost/dnd_designer",
     migrations: {
-      directory: './db/migrations'
+      directory: "./db/migrations"
     },
     seeds: {
-      directory: './db/seeds/dev'
+      directory: "./db/seeds/dev"
     },
     useNullAsDefault: true
   },
 
   test: {
-    client: 'pg',
-    connection:'postgres://localhost/dnd_designer_test',
+    client: "pg",
+    connection: "postgres://localhost/dnd_designer_test",
     migrations: {
-      directory: './db/migrations'
+      directory: "./db/migrations"
     },
     seeds: {
-      directory: './db/seeds/test'
+      directory: "./db/seeds/test"
     },
     useNullAsDefault: true
   },
 
   production: {
-    client: 'pg',
-    connection: '',
+    client: "pg",
+    connection: process.env.DATABASE_URL,
     migrations: {
-      directory: './db/migrations'
+      directory: "./db/migrations"
     },
     seeds: {
-      directory: './db/seeds/dev'
+      directory: "./db/seeds/production"
     },
     useNullAsDefault: true
   }
-}
+};
