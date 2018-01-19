bb186d0305d0a52a0a9f1826936a7c36e92555c8
Benjamin Ross <benjaminross1001@gmail.com>
2018-01-19 11:16:53 -0700

    add heroku build step

--- index.js
+++ index.js
@@ -1,24 +1,24 @@
 const express = require("express");
 const passport = require("passport");
 const cookieSession = require("cookie-session");
-const bodyParser = require('body-parser');
-const environment = process.env.NODE_ENV || 'development';
-const configuration = require('./knexfile')[environment];
-const database = require('knex')(configuration);
-const keys = require("./config/keys")
+const bodyParser = require("body-parser");
+const environment = process.env.NODE_ENV || "development";
+const configuration = require("./knexfile")[environment];
+const database = require("knex")(configuration);
+const keys = require("./config/keys");
 require("./services/passport");
 
 const app = express();
 
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(
   cookieSession({
     maxAge: 30 * 24 * 60 * 60 * 1000,
     keys: [keys.cookieKey]
   })
-)
+);
 
 app.get("/", (req, res) => {
   res.send({ hi: "there" });
@@ -29,6 +29,15 @@ app.use(passport.session());
 
 require("./routes/authRoutes")(app);
 require("./routes/characterRoutes")(app);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+
+  const path = require("path");
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
