1e2912580a44199d51c57d692dad89cd9f3b2625
Benjamin Ross <benjaminross1001@gmail.com>
2018-01-19 13:27:33 -0700

    race info styling

--- client/src/components/CharacterNew/RaceInfo/RaceInfo.js
+++ client/src/components/CharacterNew/RaceInfo/RaceInfo.js
@@ -1,54 +1,68 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 import classes from "./RaceInfo.css";
+import Aux from "../../../hoc/Aux/Aux";
 
 class RaceInfo extends Component {
-  selectRace = event => {
-
-  }
+  selectRace = event => {};
 
   render() {
     let raceInfo = <h1>No Race Selected</h1>;
 
     if (this.props.charas.character) {
       raceInfo = (
-        <div>
-          <button onClick={() => this.selectRace()}>
-            Choose {this.props.charas.character.name}
-          </button>
+        <div className={classes.RaceInfoContainer}>
+          <div className={classes.RaceInfoTitle}>
+            <h1 style={{ marginRight: "30px" }}>
+              {this.props.charas.character.name}{" "}
+            </h1>
+          </div>
 
-          <h2 className="characterName">{this.props.charas.character.name}</h2>
-          <h3 className="characterAlignment">{this.props.charas.character.alignment}</h3>
           <img
             className={classes.IMG}
             src={this.props.charas.character.image}
             alt="Character"
           />
+          <h2>{this.props.charas.character.alignment}</h2>
+          <table style={{ marginBottom: '10px'}}>
+            <thead>
+              <tr>
+                <th>Ability Score</th>
+                <th>Age</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>{this.props.charas.character.ability_score_increase}</td>
+                <td>{this.props.charas.character.max_age}</td>
+              </tr>
+            </tbody>
+          </table>
+          <table style={{ marginBottom: '10px'}}>
+            <thead>
+              <tr>
+                <th>Size</th>
+                <th>Height</th>
+                <th>Weight</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>{this.props.charas.character.size}</td>
+                <td>{this.props.charas.character.height}</td>
+                <td>{this.props.charas.character.avg_weight}</td>
+              </tr>
+            </tbody>
+          </table>
 
-          <ul>
-            <li>
-              Ability Score: {this.props.charas.character.ability_score_increase}
-            </li>
-            <li>Age: {this.props.charas.character.max_age}</li>
-            <li>
-              Physique:
-              <ul>
-                <li>Size: {this.props.charas.character.size}</li>
-                <li>Height: {this.props.charas.character.height}</li>
-                <li>Weight: {this.props.charas.character.avg_weight}</li>
-              </ul>
-            </li>
-          </ul>
-
-          <p>{this.props.charas.character.description}</p>
+          <div className={classes.RaceDescription}><p>{this.props.charas.character.description}</p></div>
+          <button onClick={() => this.selectRace()}>
+            Choose {this.props.charas.character.name}
+          </button>
         </div>
       );
     }
-    return (
-      <div className={classes.RaceInfoContainer}>
-        {raceInfo}
-      </div>
-    );
+    return <Aux>{raceInfo}</Aux>;
   }
 }
 
