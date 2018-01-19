7708a83e80bac9cdd1f706c8f94799a6e85cda94
Benjamin Ross <benjaminross1001@gmail.com>
2018-01-19 10:36:02 -0700

    change google strategy settings

--- services/passport.js
+++ services/passport.js
@@ -24,7 +24,8 @@ passport.use(
     {
       clientID: keys.googleClientID,
       clientSecret: keys.googleClientSecret,
-      callbackURL: "/auth/google/callback"
+      callbackURL: "/auth/google/callback",
+      proxy: true
     },
     async (accessToken, refreshToken, profile, done) => {
       const id = profile.id;
