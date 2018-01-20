42e8eeae0f95ed166bc64957f0d7f26ee79b4b99
Benjamin Ross <benjaminross1001@gmail.com>
2018-01-20 11:30:42 -0700

    save selections from form to state

--- client/src/components/UI/Dropdown/Dropdown.js
+++ client/src/components/UI/Dropdown/Dropdown.js
@@ -5,7 +5,7 @@ const dropdown = props => {
   return (
     <div className={classes.DropdownContainer}>
       <select disabled={props.options.length === 0} onChange={props.changed}>
-        <option disabled>Choose {props.selection}</option>
+        <option disabled selected value>Choose {props.selection}</option>
         {props.options.map(option => {
           return (
             <option key={option} value={option}>
