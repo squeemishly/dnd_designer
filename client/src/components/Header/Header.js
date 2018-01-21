bfd620a6abe9fec8cd96a8701a8080fb41618fcf
Benjamin Ross <benjaminross1001@gmail.com>
2018-01-21 14:36:43 -0700

    use react router link instead of anchor tags for non logout links in  header

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
+import { Link } from "react-router-dom";
 import classes from "./Header.css";
 
 class Header extends Component {
@@ -16,7 +17,12 @@ class Header extends Component {
       default:
         return [
           <li key="1">Welcome {this.props.auth.username}</li>,
-          <li key="3"><a href="/character/new">New Character</a></li>,
+          <li key="4">
+            <Link to="/character/list">Character List</Link>
+          </li>,
+          <li key="3">
+            <Link to="/character/new">New Character</Link>
+          </li>,
           <li key="2">
             <a href="/api/logout">Logout</a>
           </li>
