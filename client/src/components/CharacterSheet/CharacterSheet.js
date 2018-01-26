e252f2a7c24f5f54ff67bd7db6b514f1a9cc327c
Ben Ross <benjaminross1001@gmail.com>
2018-01-26 12:21:30 -0700

    Char sheet layout (#12)
    create basic layout for character list and character sheet

--- client/src/components/CharacterSheet/CharacterSheet.js
+++ client/src/components/CharacterSheet/CharacterSheet.js
@@ -4,9 +4,19 @@ import classes from "./CharacterSheet.css";
 class CharacterSheet extends Component {
   render() {
     return (
-      <div className={classes.CharacterSheetContainer}>
-        <h1>CHARACTER SHEET</h1>
-        <h2>{this.props.character}</h2>
+      <div className={classes.Container}>
+        <div className={classes.Title}>Title</div>
+        <div className={classes.AttributeContainer}>
+          <div className={classes.STRContainer}> STR DEX CON ZNT</div>
+          <div className={classes.ACContainer}>AC CNLT HP</div>
+        </div>
+        <div className={classes.SkillsWeaponBackPackContainer}>
+          <div className={classes.SkillsContainer}>Skills</div>
+          <div className={classes.WeaponsBackpackContainer}>
+            <div className={classes.WeaponsContainer}>Weapons</div>
+            <div className={classes.BackpackContainer}>Backpack</div>
+          </div>
+        </div>
       </div>
     );
   }
