ab97fe00a93819df859a2e818d07f3626114a20e
squeemishly <squeemishly@gmail.com>
2018-02-05 16:36:01 -0700

    change getRaceDetails to getCharacterOptions

--- routes/characterRoutes.js
+++ routes/characterRoutes.js
@@ -3,7 +3,7 @@ const CharactersController = require("../lib/controllers/charactersController");
 module.exports = app => {
   app.get("/api/character/race/:name", CharactersController.getRace)
 
-  app.get('/api/character/details/:race', CharactersController.getRaceDetails)
+  app.get('/api/character/details/:race', CharactersController.getCharacterOptions)
 
   app.post('/api/character/new', CharactersController.postCharacter)
 }
